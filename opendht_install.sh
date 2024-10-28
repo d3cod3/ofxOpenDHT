8d160406a330ec4632d8ae23b29f28719f6dcc86
d3cod3 <n3m3da@d3cod3.org>
2024-10-28 09:55:51 +0100

    added opendht linux install script

---
+++ opendht_install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+########################################################################
+#
+# openDHT library install & compile script
+#
+# OpenDHT library github:
+# https://github.com/savoirfairelinux/opendht
+#
+########################################################################
+
+
+########################################################################
+# Check if running with sudo
+if [ $(id -u) != 0 ]; then
+    echo -e "\nthis installer script must be run with super-user privileges."
+    echo -e "\nUsage:\nsudo "$0"\n"
+    exit $exit_code
+    exit 1
+fi
+
+########################################################################
+# Install OpenDHT dependencies
+sudo apt install build-essential git cmake libncurses5-dev libreadline-dev nettle-dev libgnutls28-dev libargon2-0-dev libmsgpack-dev libssl-dev libfmt-dev libjsoncpp-dev libhttp-parser-dev libasio-dev libcppunit-dev
+
+########################################################################
+# clone the repo
+cd lib
+git clone https://github.com/savoirfairelinux/opendht.git
+
+########################################################################
+# build and install
+cd opendht
+mkdir build && cd build
+cmake -DOPENDHT_PYTHON=OFF -DCMAKE_INSTALL_PREFIX=/usr ..
+make -j4
+sudo make install
+
+########################################################################
+# copy lib pkgconfig reference
+sudo cp /usr/lib/x86_64-linux-gnu/pkgconfig/opendht.pc /usr/local/lib/pkgconfig/opendht.pc
+
